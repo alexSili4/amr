ccc70b32f11d4268d29772c2e94059fa33a45382
Silich Alexander <124524007+sili41995@users.noreply.github.com>
2024-08-09 15:01:11 +0300

    add consultation

--- src/js/eventHandlers.js
+++ src/js/eventHandlers.js
@@ -1,6 +1,7 @@
 import refs from './refs';
 
 refs.questionsList.addEventListener('click', toggleShowAnswer);
+refs.consultationForm.addEventListener('input', onConsultationFormInput);
 
 function toggleShowAnswer(e) {
   const targetClassName = 'hidden-desc';
@@ -12,4 +13,26 @@ function toggleShowAnswer(e) {
 
   const questionContainer = e.target.closest('.js-landing-questions-list-question-wrap');
   questionContainer.classList.toggle(targetClassName);
+}
+
+function onConsultationFormInput(e) {
+  const data = {};
+
+  const formData = new FormData(e.currentTarget);
+
+  formData.forEach((value, key) => {
+    data[key] = value;
+  });
+
+  const dataKeys = Object.keys(data);
+  const isValidDataLength = dataKeys.length === 4;
+  const isNotEmptyForm = dataKeys.every((key) => data[key]);
+
+  const isDisabled = !isValidDataLength || !isNotEmptyForm;
+
+  if (isDisabled) {
+    refs.consultationFormSubmitBtn.setAttribute('disabled', '');
+  } else {
+    refs.consultationFormSubmitBtn.removeAttribute('disabled');
+  }
 }
--- src/js/refs.js
+++ src/js/refs.js
@@ -1,5 +1,7 @@
 const refs = {
   questionsList: document.querySelector('.js-landing-questions-list'),
+  consultationForm: document.querySelector('.js-landing-consultation-form'),
+  consultationFormSubmitBtn: document.querySelector('.js-landing-consultation-form-submit-btn'),
 };
 
 export default refs;
